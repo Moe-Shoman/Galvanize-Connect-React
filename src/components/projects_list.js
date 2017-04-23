22baefe572700324e176313ddbab52a15928f4b2
Ivonne Terrero <iqcodes21@gmail.com>
2017-04-23 16:22:15 -0700

    comps folder

---
+++ src/components/projects_list.js
@@ -0,0 +1,14 @@
+import React {component} from 'react';
+
+
+
+const ProjectsList = () => {
+  return (
+    <ul>
+      <li>Project 1</li>
+      <li>Project 2</li>
+      <li>Project 3</li>
+      <li>Project 4</li>
+    </ul>
+  )
+}
