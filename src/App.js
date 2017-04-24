55633373a1a7f2b6a7f5ca085646864438dfd3d3
Ivonne Terrero <iqcodes21@gmail.com>
2017-04-23 20:17:52 -0700

    attempted to add semantic-ui

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import React, { Component } from 'react';
-
+import LogInForm from './logIn';
 
 class App extends Component {
-
+  constructor(){
+    super()
+  }
   render() {
     return (
       <div className="App">
         <div className="App-header">
           <h2>Galvanize-Connect 1</h2>
+          <LogInForm/>
         </div>
       </div>
     );
