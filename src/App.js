161260adecae1da672a49f6827c03bfb672a722a
Mo-Sho <muhama94@gmail.com>
2017-04-24 16:21:39 -0700

    OAuth returns token

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
 import React, { Component } from 'react';
 import GoogleAuth from './components/GoogleAuth/GoogleAuth';
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
-
+import firebase from 'firebase';
 class App extends Component {
-  constructor(){
-    super()
+  constructor(props){
+    super(props)
+  }
+  componentWillMount(){
+    let config = {
+        apiKey: "AIzaSyBcZY-ngAyYe9-gw5z_A0_k6so4BPsJ8V0",
+        authDomain: "galvanize-connect-cfd11.firebaseapp.com",
+        databaseURL: "https://galvanize-connect-cfd11.firebaseio.com",
+        projectId: "galvanize-connect-cfd11",
+        storageBucket: "galvanize-connect-cfd11.appspot.com",
+        messagingSenderId: "836129324682"
+    }
+    firebase.initializeApp(config)
   }
   render() {
     return (
--- src/components/GoogleAuth/GoogleAuth.js
+++ src/components/GoogleAuth/GoogleAuth.js
@@ -1,11 +1,20 @@
 import React, {Component} from 'react';
-
+import firebase from 'firebase';
+let provider = new firebase.auth.GoogleAuthProvider();
+provider.addScope('profile');
+provider.addScope('email');
 export default class GoogleAuth extends Component {
+  signIn = () =>{
+    return firebase.auth().signInWithPopup(provider)
+      .then((res) => {
+        console.log('res', res);
+      })
+  }
     render() {
       return (
         <div>
           <h2>SIGN IN WITH GOOGLE</h2>
-          <button>GOOGLE</button>
+          <button onClick={this.signIn}>GOOGLE</button>
         </div>
       )
     }
