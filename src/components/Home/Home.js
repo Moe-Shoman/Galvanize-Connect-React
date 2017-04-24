f758f822520819e53ddc764705ea29ff6392aa40
Gurdip Singh <gurdipsingh1990@gmail.com>
2017-04-24 15:01:31 -0700

    Added react/firebase chat app component

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,11 +1,65 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
+var firebase = require('firebase');
 
-export default class Home extends Component {
-    render() {
-      return (
-        <div>
-          <h2>Home Component is up!!</h2>
-        </div>
-      )
-    }
+
+class Home extends Component {
+ constructor(props, context) {
+  super(props, context)
+  this.updateMessage = this.updateMessage.bind(this)
+  this.submitMessage = this.submitMessage.bind(this)
+   this.state = {
+    message: '',
+    messages: []
+   }
+ }
+
+ componentDidMount() {
+  firebase.database().ref('messages/').on('value', (snapshot) => {
+   const currentMessages = snapshot.val()
+
+   if(currentMessages != null) {
+    this.setState({
+     messages: currentMessages
+    })
+   }
+  })
+ }
+
+ updateMessage(event) {
+  this.setState({
+   message: event.target.value
+  })
+ }
+
+ submitMessage(event) {
+   const nextMessage = {
+    id: this.state.messages.length,
+    text: this.state.message
+   }
+   let list = Object.assign([], this.state.messages)
+   list.push(nextMessage)
+   this.setState({
+    messages: list
+   })
+ }
+
+ render() {
+  const currentMessage = this.state.messages.map((message, i) => {
+   return (
+    <li key={message.id}>{message.text}</li>
+   )
+  })
+  return (
+   <div>
+    <ol>
+     {currentMessage}
+    </ol>
+    <input onChange={this.updateMessage} type="text" placeholder="Message" />
+    <br/>
+    <button onClick={this.submitMessage}>Subtmit Message</button>
+   </div>
+  )
+ }
 }
+
+export default Home;
