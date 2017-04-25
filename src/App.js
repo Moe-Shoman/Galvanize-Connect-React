4e31109e72cba181909ae197ba1c38e8c1e25c9d
Ivonne Terrero <iqcodes21@gmail.com>
2017-04-25 10:57:07 -0700

    exact

--- src/App.js
+++ src/App.js
@@ -17,28 +17,20 @@ class App extends Component {
   render() {
     return (
     <div class="ui five item menu">
-      {/* <div>
-      <LogInForm / >
-      </div> */}
+
      <Router class="ui five item menu">
       <div>
        <SideNav/>
        <Route  class="item" path='/Home' component={Home}/>
        <Route class="item" path='/sign-in' component={GoogleAuth}/>
        <Route class="item" path='/Cohort' component={Cohort}/>
-       <Route class="item" path='/Profile' component={Profile}/>
+       <Route exact class="item" path='/Profile' component={Profile}/>
       </div>
      </Router>
     </div>
     );
   }
 }
-{/* <div class="ui five item menu">
-      <a class="item" [routerLink]="['/home']">Home</a>
-      <a class="item" [routerLink]="['/login']">LogIn</a>
-      <a class="item" [routerLink]="['/register']">Register</a>
-      <a class="item" [routerLink]="['/mainsearch']">Search Ingredients</a>
-      <a class="item" [routerLink]="['/talkapi']">HTTP-Example</a>
-    </div> */}
+
 
 export default App;
