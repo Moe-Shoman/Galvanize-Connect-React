894dfb8a1eb002af3b90bb366ee0cbe63a29c3b4
Gurdip Singh <gurdipsingh1990@gmail.com>
2017-04-26 14:18:42 -0700

    Modal is taking in input and  console logging, beginning refactor to redux

--- src/components/ReplyToPost/ReplyToPost.js
+++ src/components/ReplyToPost/ReplyToPost.js
@@ -1,16 +1,31 @@
-import React from 'react'
+import React, { Component } from 'react'
 import { Button, Modal } from 'semantic-ui-react'
 
-const ReplyToPost = () => (
+class ReplyToPost extends Component {
+ constructor(props) {
+  super(props);
+  this.state = {
+   reply: '',
+  }
+ }
+ updateReply= (e) =>  {
+  console.log(e.target.value)
+
+ }
+ render() {
+ return (
   <Modal
     trigger={<Button>Reply</Button>}
     header='Post Reply'
-    content='Message...'
+    content={<input/>}
+    value={this.state.reply}
     actions={[
       { key: 'no', content: 'No', color: 'red', triggerClose: true },
       { key: 'yes', content: 'Yes', color: 'green', triggerClose: true },
     ]}
+    onChange={this.updateReply}
   />
-)
-
+ )
+}
+}
 export default ReplyToPost
