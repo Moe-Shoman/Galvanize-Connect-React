b0c1a30726188ea77c322baa464e1f59a43d2759
Gurdip Singh <gurdipsingh1990@gmail.com>
2017-04-26 11:18:12 -0700

    Saving changes Wed morning to pull from master

--- src/components/Article/Article.js
+++ src/components/Article/Article.js
@@ -1,19 +1,41 @@
 import React, {Component} from 'react'
 import {Button} from 'react-bootstrap';
 
-class Article extends Component {
- constructor(props) {
-  super(props)
- }
- render() {
-  return (
-   <div>
-    <input onChange={this.updateMessage} type="text" placeholder="Message" />
-    <br/>
-    <button onClick={this.submitMessage}>What's Up?</button>
-   </div>
-  )
- }
+// class Article extends Component {
+//
+//  logLink = (e) => {
+//   e.preventDefault();
+//   console.log('it worked?', e.target);
+//  }
+//
+//  render() {
+//   return (
+//    <div>
+//     <input onSubmit={this.logLink} type="text" placeholder="Post Link" />
+//     <button>Click</button>
+//    </div>
+//   )
+//  }
+// }
+//
+// export default Article;
+
+
+class Article extends React.Component {
+  // This syntax ensures `this` is bound within handleClick.
+  // Warning: this is *experimental* syntax.
+  handleClick = () => {
+    console.log('this is:', this);
+  }
+
+  render() {
+    return (
+      <button onClick={this.handleClick}>
+        Click me
+      </button>
+    );
+  }
 }
+
 
 export default Article;
