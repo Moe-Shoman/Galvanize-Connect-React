9f97e28a4938107ca6d1b67f4e2aa2e14c5ad535
Gurdip Singh <gurdipsingh1990@gmail.com>
2017-04-28 09:36:00 -0700

    Saving changes friday morning to merge latest chagnes

--- src/components/Home/Comments.js
+++ src/components/Home/Comments.js
@@ -1,19 +1,55 @@
-import React, {Component} from 'react';
-import { reduxForm, Field  } from 'redux-form';
-import { connect } from 'react-redux';
-import { bindActionCreators } from 'redux';
+// import React, {Component} from 'react';
+// import { reduxForm, Field  } from 'redux-form';
+// import { connect } from 'react-redux';
+// import { bindActionCreators } from 'redux';
+//
+//
+// function mapStateToProps ({GoogleAuth, comments, form  }) {
+//  return {
+//   GoogleAuth,
+//   posts,
+//   form
+//  }
+// }
+//
+// function mapDispatchToProps (dispatch) {
+//  return bindActionCreators({ addComment }, dispatch)
+// }
+//
+//
+// class Comments extends Component {
+//     render() {
+//         return (
+//             <div>
+//                 <p>I'm a Comment</p>
+//             </div>
+//         )
+//     }
+// }
+
+
+// export default Comments;
 
 
 
-class Comments extends Component {
-    render() {
-        return (
-            <div>
-                <p>I'm a Comment</p>
-            </div>
-        )
-    }
-}
 
 
-export default Comments;
+// class PostForm extends Component {
+//     render() {
+//      const { GoogleAuth, addPost, handleSubmit, form } = this.props;
+//         return (
+//          <form>
+//            <div>
+//              <label htmlFor="post">Add Post</label>
+//              <Field  name="post" component="input" type="text"/>
+//            </div>
+//            <button type="submit" onClick={(e) => {e.preventDefault(); addPost(GoogleAuth, form)}}>Submit</button>
+//          </form>
+//         )
+//     }
+// }
+//
+//
+// export default connect(mapStateToProps, mapDispatchToProps)(reduxForm({
+//  form: 'PostForm',
+// })(PostForm))
