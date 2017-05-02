2b34f6d974512f78f9802254c3ffce845cf5b9fc
Mo-Sho <muhama94@gmail.com>
2017-05-02 10:45:52 -0700

    toggle add project form

--- src/components/AddProject/add_project_form.js
+++ src/components/AddProject/add_project_form.js
@@ -1,57 +1,63 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import './projectform.css';
 // import {Field, Form, Group, Button, Input, TextArea} from 'semantic-ui-react';
-import { bindActionCreators } from 'redux';
-import { addProject } from '../../actions';
-import { connect } from 'react-redux';
+import {bindActionCreators} from 'redux';
+import {addProject} from '../../actions';
+import {connect} from 'react-redux';
 
-
-
-
-
-function mapStateToProps({ userData, projects}){
-  return {
-    userData,
-    projects
-  }
+function mapStateToProps({userData, projects}) {
+    return {userData, projects}
 }
 
 class AddProjectForm extends Component {
-  constructor(props){
-    super(props)
-    this.state = {
-        projectName: '',
-        description: ''
+    constructor(props) {
+        super(props)
+        this.state = {
+            projectName: '',
+            description: '',
+            showForm: false
+        }
     }
-  }
-  updateInput = (event) => {
-    const value = event.target.value;
-    const name = event.target.name
-    this.setState({
-      [name]: value
-    })
-  }
-  render() {
-   const { userData, addProject, form } = this.props;
-      return (
-       <form className="theForm ui form">
-         <div>
-           <div calssName="field">
+    updateInput = (event) => {
+        const value = event.target.value;
+        const name = event.target.name
+        this.setState({[name]: value})
+    }
+    toggleForm = () => {
+        this.setState((prevState) => {
+          return {showForm: !prevState.showForm}
+        })
+    }
+    render() {
+      const {userData, addProject, form} = this.props;
+      if (this.state.showForm) {
+        return(
+        <form className="theForm ui form">
+          <div>
+            <div calssName="field">
               <label htmlFor="projectName">Project Name</label>
               <input className="proName" name="projectName" onChange={this.updateInput} type="text"/>
-              </div>
+            </div>
             <div>
               <label htmlFor="description">Project Description</label>
               <input className="proDesc" name="description" onChange={this.updateInput} type="text"/>
             </div>
           </div>
-         <button className="ui button" type="submit" onClick={(e) => {e.preventDefault(); addProject( userData, {projectName: this.state.projectName, description: this.state.description})}}>Submit</button>
-       </form>
-      )
-  }
+          <button className="ui button" type="submit" onClick={(e) => {
+            e.preventDefault();
+            addProject(userData, {
+              projectName: this.state.projectName,
+              description: this.state.description
+            })
+          }}>Submit</button>
+        </form>)
+      }
+        return (
+            <div>
+                <button onClick={this.toggleForm}>ADD PROJECT</button>
+            </div>
+        )
+    }
 }
 
-
-
-
-export default connect(mapStateToProps, { addProject })(AddProjectForm)
+export default connect(mapStateToProps, {addProject})(AddProjectForm)
