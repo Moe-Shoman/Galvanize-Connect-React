db609ebe903814555107cd54e3c14c77ecc6be7d
Gurdip Singh <gurdipsingh1990@gmail.com>
2017-05-03 17:31:59 -0700

    semanics cards showing, content coming soon

--- src/components/Cohort/MemberCards.js
+++ src/components/Cohort/MemberCards.js
@@ -2,7 +2,7 @@ import React, {Component} from 'react';
 import {connect} from 'react-redux';
 import firebase from 'firebase'
 import {fetchCohort} from '../../actions';
-import { Card, Icon, Image } from 'semantic-ui-react'
+import {Card, Icon, Image} from 'semantic-ui-react'
 
 function mapStateToProps({cohortVal}) {
     return {cohortVal}
@@ -18,29 +18,44 @@ class MemberCards extends Component {
         })
     }
 
-    renderCohorMember = (cohortMembers) => {
-     if(cohortMembers) {
-      const members = Object.values(cohortMembers);
-      return members.map((member) => {
-       console.log('member inside of renderCohorMember is ', member.photo);
-      })
-     }
-    }
+    // renderCohorMember = (cohortMembers) => {
+    //  if(cohortMembers) {
+    //   const members = Object.values(cohortMembers);
+    //   return members.map((member) => {
+    //    console.log('data after renderCohorMember ', member)
+    //   })
+    //  }
+    // }
 
     render() {
-      const cohortMems = this.props.cohortVal.map((member) => {
-       return (
-        this.renderCohorMember(member)
-       )
-      })
+      // console.log('cohort val is ', this.props.cohortVal);
+      const cards = [];
+      for (let obj in this.props.cohortVal[0]) {
+       console.log('NAME:', obj.name);
+       console.log('SRC:', obj.photo);
+       console.log('obj:', obj);
+        cards.push(
+         <Card>
+            <Image src={obj.photo}/>
+            <Card.Content>
+                <Card.Header>{obj.name}</Card.Header>
+            </Card.Content>
+        </Card>
+        )
+      }
+        // const cohortMems = this.props.cohortVal.map((member) => {
+        //     return (
+        //         <Card>
+        //             <Image src={member.photo}/>
+        //             <Card.Content>
+        //                 <Card.Header>{member.name}</Card.Header>
+        //             </Card.Content>
+        //         </Card>
+        //     );
+        // });
 
         return (
-         <Card>
-             <Image src={cohortMems.photo}/>
-             <Card.Content>
-                 <Card.Header>{cohortMems.name}</Card.Header>
-             </Card.Content>
-         </Card>
+         <div>{cards}</div>
         )
     }
 }
