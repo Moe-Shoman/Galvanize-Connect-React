d26700dd954b7e087b8f4c255dc7d4fc3622f9cf
Gurdip Singh <gurdipsingh1990@gmail.com>
2017-05-05 11:09:45 -0700

    Cleaned up cohort component

--- src/components/Cohort/MemberCards.js
+++ src/components/Cohort/MemberCards.js
@@ -20,48 +20,20 @@ class MemberCards extends Component {
         firebase.database().ref(`cohorts/${this.props.userData.cohort}`).once("value", (snapshot) => {
             return this.props.fetchCohort(snapshot.val());
         })
-        // firebase.database().ref('cohorts').once("value", (snapshot) => {
-        //     return this.props.fetchCohort(snapshot.val());
-        // })
     }
-
-    // renderCohorMember = (cohortMembers) => {
-    //  console.log('cohortMembers is =============== ', cohortMembers);
-    //     if (cohortMembers) {
-    //         const members = Object.values(cohortMembers);
-    //         return members.map((member) => {
-    //          console.log('memerb is ============== ', member);
-    //             return (
-    //                 // <div class="card">
-    //                   <Card>
-    //                       <Image src={member.photo}/>
-    //                       <Card.Content>
-    //                         <Card.Header>{member.name}</Card.Header>
-    //                       </Card.Content>
-    //                   </Card>
-    //                 // </div>
-    //             )
-    //         })
-    //     }
-    // }
 
     render() {
          const members = Object.values(this.props.cohortVal);
         const cohortUser = members.map((cohort) => {
-            // return (
-            //     <div>
-            //         {this.renderCohorMember(cohort)}
-            //     </div>
-            // )
             return (
-                // <div class="card">
+                <div class="card">
                   <Card>
                       <Image src={cohort.photo}/>
                       <Card.Content>
                         <Card.Header>{cohort.name}</Card.Header>
                       </Card.Content>
                   </Card>
-                // </div>
+                </div>
             )
         })
         return (
