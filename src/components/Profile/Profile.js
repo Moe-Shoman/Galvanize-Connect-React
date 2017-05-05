72e5371e6665c90c400f85e40d1cd0ea8b28691d
Ivonne Terrero <iqcodes21@gmail.com>
2017-05-05 10:54:59 -0700

    removed unnecessary lines

--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -1,11 +1,9 @@
 import React, {Component} from 'react';
-// import semantic from 'semantic-ui-react';
 import './profile.css';
 import ProjectsList from '../Projects/projects_list';
 import SkillsList from '../Skills/skills_list';
 import AddProjectForm from '../AddProject/add_project_form';
 import { connect } from 'react-redux';
-// import firebase from 'firebase';
 import AddSkillsForm from '../Skills/skills_form';
 import AddCohort from './add_cohort';
 import SocialLinks from './add_social';
