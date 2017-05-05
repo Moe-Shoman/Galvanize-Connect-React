a2af944326e80a9757d40c8277a810b9489b9a9c
Gurdip Singh <gurdipsingh1990@gmail.com>
2017-05-05 10:30:18 -0700

    Semantics card is showing up, but twice

--- src/components/Cohort/Cohort.js
+++ src/components/Cohort/Cohort.js
@@ -1,14 +1,17 @@
 import React, {Component} from 'react';
 import MemberCards from './MemberCards'
+import {Sidebar} from 'semantic-ui-react'
 
 export default class Cohort extends Component {
     render() {
       return (
+       <Sidebar.Pusher>
         <div>
          <div>
           <MemberCards/>
          </div>
         </div>
+       </Sidebar.Pusher>
       )
     }
 }
