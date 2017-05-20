3dea8c7661927e1b2f70849a0484492789e78ed7
Gurdip Singh <gurdipsingh1990@gmail.com>
2017-05-20 12:23:36 -0700

    cut out new branch

--- src/components/Cohort/Cohort.js
+++ src/components/Cohort/Cohort.js
@@ -2,20 +2,6 @@ import React, { Component } from 'react';
 import MemberCards from './MemberCards';
 import { Sidebar } from 'semantic-ui-react';
 
-// export default class Cohort extends Component {
-//   render() {
-//     return (
-//       <Sidebar.Pusher>
-//         <div>
-//           <div>
-//             <MemberCards />
-//           </div>
-//         </div>
-//       </Sidebar.Pusher>
-//     );
-//   }
-// }
-
 const cohort = () => (
   <Sidebar.Pusher>
     <div>
