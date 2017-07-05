183ba57b4f7427eee0b44fbf04aeab9c04df2915
Gurdip Singh <gurdipsingh1990@gmail.com>
2017-07-04 18:45:02 -0700

    Added contact me paragraph on welcome page

--- src/components/Learn/LearnMore.js
+++ src/components/Learn/LearnMore.js
@@ -11,7 +11,11 @@ const LearnMore = () => (
     <div className="description">
       <p className="welcomeparagraph">
         Welcome to Galvanize-Connect. The best way to maintain communication with your
-        Galvanize friends, share your skills and projects, and event get some tech news.
+        Galvanize friends, share your skills & projects, and event get tech news.
+       </p>
+
+      <p>
+        This website is constantly improving. New features are being added weekly, while other features are being refactored. If you have any questions, or suggestions, email me at Gurdipsingh1990@gmail.com
        </p>
     </div>
   </div>
